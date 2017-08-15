3168f2e01309340a8fc3a1fa7380ed578d32079d
bumbaro2217 <byron2217@gmail.com>
2017-08-15 08:19:24 -0400

    objects

--- task.js
+++ task.js
@@ -3,9 +3,24 @@ var task = {
     description: 'My description'
 };
 
-task.toString = function() {
-    return this.title + ' ' + this.description;
-}
+Object.defineProperty(task, 'toString', {
+    value: function() {
+        return this.title + ' ' + this.description;
+    },
+    writable: false,
+    enumberable: false,
+    configurable: false
+});
 
-console.log(task.toString());
+var urgentTask = Object.create(task);
+Object.defineProperty(urgentTask, 'toString', {
+    value: function() {
+        return this.title + ' is urgent';
+    },
+    writable: false,
+    enumberable: false,
+    configurable: false
+});
+
+console.log(urgentTask.toString());
 
